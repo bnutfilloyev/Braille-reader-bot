cb5b8fcfe5863610c1248369975132d1c98c7020
Bexruz Nutfilloyev <63140958+YoshlikMedia@users.noreply.github.com>
2021-09-14 10:26:39 +0500

    Add APP_ID, APP_KEY

--- data/config.py
+++ data/config.py
@@ -4,6 +4,8 @@ from environs import Env
 env = Env()
 env.read_env()
 
-BOT_TOKEN = env.str("BOT_TOKEN")  # Забираем значение типа str
-ADMINS = env.list("ADMINS")  # Тут у нас будет список из админов
-IP = env.str("ip")  # Тоже str, но для айпи адреса хоста
+BOT_TOKEN = env.str("BOT_TOKEN")
+ADMINS = env.list("ADMINS")
+IP = env.str("ip")
+APP_ID = env.str("APP_ID")
+APP_KEY = env.str("APP_KEY")
